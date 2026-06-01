2e4cc5d3ba429540e0668d09fc784c892c90ea31
srilaxmiankena2001-maker <srilaxmiankena2001@gmail.com>
2026-06-01 10:56:14 -0400

    Add versioning configuration to S3 bucket

--- s3.tf
+++ s3.tf
@@ -1,10 +1,11 @@
-# Create an S3 Bucket
 resource "aws_s3_bucket" "example_bucket" {
   bucket = "swiggy-bucket-terraform-0099demo"
-
-  # Enable versioning
-  versioning {
-    enabled = true
-  }
 }
 
+resource "aws_s3_bucket_versioning" "example_bucket_versioning" {
+  bucket = aws_s3_bucket.example_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
